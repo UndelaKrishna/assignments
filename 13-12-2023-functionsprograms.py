030925b179e7b4d32012c842c068f8aec91499ab
UndelaKrishna <150779501+UndelaKrishna@users.noreply.github.com>
2023-12-15 10:28:30 +0530

    Update 13-12-2023-functionsprograms.py

--- 13-12-2023-functionsprograms.py
+++ 13-12-2023-functionsprograms.py
@@ -1,5 +1,4 @@
 #lambda
-# syntax:-lambda input:expression
 #1
 a=lambda n:n*n
 print(a(5))
@@ -12,24 +11,17 @@ l1=lambda a :a+10
 print(l1(10))
 
 #4
-
-
 str1 = 'pythonisdynamiclanguage'
- 
 upper = lambda string: string.upper()
 print(upper(str1))
 
-
 #5
-
-
 ages = [13, 90, 17, 59, 21, 60, 5]
-l1= list(filter(lambda age: age > 18, ages))
- 
+l1= list(filter(lambda age: age > 18, ages)) 
 print(l1)
-#filter
 
-#syntax:-filter(function,sequence)
+#filter
+#1
 def even(x):
     if x%2==0:
         return True
@@ -38,15 +30,8 @@ def even(x):
 l=[1,2,3,4,5,6,7,8,9,10]
 a=filter(even,l)
 print(a)
-# for i in a:
-#     print(i)
-# b=filter(even,l)
-# print(b)
 
-
-
-#filter
-#1
+#2
 def even(x):
     if x%2==0:
         return  True
@@ -56,8 +41,7 @@ l1=[1,20,30,40,22,13,12,44,55,18]
 a=list(filter(even,l1))
 print(a)
 
-print("_________-")
-#2
+#3
 def fact(n):
     fa=1
     for i in range(1,n+1):
@@ -71,14 +55,15 @@ for i in b:
     print(i)
 
 
-#3
+#4
 list=[1,22,33,2,44,14,19,50,22]    
 b=filter(lambda a:a%2==0,list)
 print(b)
 for i in b:
     print(i)
-print("_______")
-#4
+
+
+#5
 def myFunc(x):
   if x < 18:
     return False
@@ -87,11 +72,10 @@ def myFunc(x):
 l1=[10,20,30,40,1,2,3,4,8,9,55]
 a=filter(myFunc,l1)
 print(a)
-
 for i in a:
    print(i,end=" ")
 
-#5
+#6
 def div(x):
    sum=0
    for i in range(1,x+1):
@@ -103,11 +87,6 @@ print(a)
 for i in a:
    print(i)  
 
-# filter function in oneline
-# l=[1,2,3,4,5,6,7,8]
-# a=filter(lambda a:a%2==0,l)
-# print(list(a))
-
 
 #map
 l1=[1,2,33,44,66,77]
@@ -117,7 +96,6 @@ print(a)
 for i in a:
    print(i)
 
-
 #2
 l1=[1,2,33,44,66,77]
 l2=[10,22,39,44,55,67,9,96]
@@ -125,6 +103,7 @@ a=map(lambda a,b:a+b,l1,l2)
 print(a)
 for i in a:
    print(i)
+ 
 #3
 l1=[1,2,33,44,66,77]
 l2=[10,22,39,44,55,67,9,96]
@@ -139,6 +118,7 @@ a=map(lambda a,b:a**b,l1,l2)
 print(a)
 for i in a:
    print(i)
+ 
 #5
 l1=[1,2,33,44,66,77]
 l2=[10,22,39,44,55,67,9,96]
@@ -146,12 +126,89 @@ a=map(lambda a,b:a//b,l1,l2)
 print(a)
 for i in a:
    print(i)
-#6l1=[1,2,33,44,66,77]
+ 
+#6
+l1=[1,2,33,44,66,77]
 l2=[10,22,39,44,55,67,9,96]
 a=map(lambda a:a%2==0,l2)
 print(a)
 for i in a:
-   print(i)                  
+   print(i) 
+
+ #recursive fun
+def fac(x):
+    if x==1:
+        result=1
+    else:
+        result=x*fac(x-1)
+    return result
+print(fac(6))
+
+def fac(x):
+    if x==1:
+        result=8
+    else:
+        result=x*fac(x-1)
+    return result
+print(fac(8))
+
+def fac(x):
+    if x==1:
+        result=0
+    else:
+        result=x*fac(x-1)
+    return result
+print(fac(6))
+
+def fac(x):
+    if x==1:
+        result=5
+    else:
+        result=x*fac(x-1)
+    return result
+print(fac(10))
+
+def fac(x):
+    if x==1:
+        result=10
+    else:
+        result=x*fac(x-1)
+    return result
+print(fac(10)) 
+
+#arguments
+def f(arg1,arg2,arg3=4,arg4=8):
+    print(arg1,arg2,arg3,arg4) 
+f(3,5)
+f(10,20,30,40)
+f(11,6,arg4=1000)
+f(arg4=6,arg1=7,arg2=9)
+f(arg3=90,arg4=80,10,50)    
+f(6,8,arg2=89)  
+f(4,5,arg3=7,arg5=78)  
+f()  
+
+#reduce
+t=(1,4.5,36,38,6,2)
+print(reduce(lambda a,b:a+b-a*b+a*a,t))
+
+from functools import reduce
+value = reduce(lambda a, b: a + b, [1,2,3,4,5])
+print(value)
+
+l=[1,4.5,36,38,6,2]
+print(reduce(lambda a,b:a<b,t))
+
+t=(1,4.5,36,38,6,2)
+print(reduce(lambda a,b:a%2!=0,t))
+
+a=[2,6,18,9,83,72,90]
+print(reduce(lambda x,y:y==2,a))
+
+
+
+
+
       
 
 
